f9ee7583a1471116b0f7773c1ceac924afb08dc4
Samuel Kobe <samuelkobe@gmail.com>
2013-04-29 16:58:25 -0700

    added spree and setup intial gems for initial push

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pa::Application.config.secret_token = '56619ea6b40954a8c7ae772ac69e7e757791126c28f5c8c7d764799a01aab512a8a4de9ec795703b2980af420d9f4a91275251396def6d93e4070a4879e3b6c0'
