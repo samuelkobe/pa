5be99a548b0b745a092d792cf010473be70c20fd
Samuel Kobe <samuelkobe@gmail.com>
2013-05-30 02:06:51 -0700

    added defacing to store index page

--- app/overrides/application_layout.rb
+++ app/overrides/application_layout.rb
@@ -1,10 +1,15 @@
+#Head Defaces
+Deface::Override.new(:virtual_path => "spree/layouts/spree_application",
+:insert_bottom                     => "[data-hook=inside_head]",
+:text                              => "<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700,400italic' rel='stylesheet' type='text/css'>",
+:name                              => "googleFontLoadedInHead")
 #Footer Defaces
 Deface::Override.new(:virtual_path => "spree/shared/_footer",
 :insert_after                      => "#footer-right",
 :text                              => "<script>$('img').lazyload();</script>",
-:name                              => "lazyload")
+:name                              => "lazyLoad")
 #Footer Defaces
 Deface::Override.new(:virtual_path => "spree/layouts/admin",
 :insert_bottom                     => ".admin",
 :text                              => "<script>$('img').lazyload();</script>",
-:name                              => "lazyload2")
+:name                              => "lazyLoadBackend")
