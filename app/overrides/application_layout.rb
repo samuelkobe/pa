016d669092537a1395ff14e4a0d629e3f4a0a114
Samuel Kobe <samuelkobe@gmail.com>
2013-05-29 22:18:49 -0700

    added lazyload to the admin backend

--- app/overrides/application_layout.rb
+++ app/overrides/application_layout.rb
@@ -1,4 +1,10 @@
-Deface::Override.new(:virtual_path  => "spree/shared/_footer",
-                     :insert_after       => "#footer-right",
-                     :text          => "<script>$('img').lazyload();</script>",
-                     :name          => "lazyload")
+#Footer Defaces
+Deface::Override.new(:virtual_path => "spree/shared/_footer",
+:insert_after                      => "#footer-right",
+:text                              => "<script>$('img').lazyload();</script>",
+:name                              => "lazyload")
+#Footer Defaces
+Deface::Override.new(:virtual_path => "spree/layouts/admin",
+:insert_bottom                     => ".admin",
+:text                              => "<script>$('img').lazyload();</script>",
+:name                              => "lazyload2")
