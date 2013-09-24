baed32a1b08c27c1baaa6ef614ab0e5b5c2b9e33
Samuel Kobe <samuelkobe@gmail.com>
2013-09-24 00:20:56 -0700

    added to retail

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Pa::Application.routes.draw do
   get "home/index"
   match '/social/' => 'home#social', as: 'social_home'
   match '/team/' => 'home#team', as: 'team_home'
+  match '/retail/' => 'home#retail', as: 'retail_home'
   match 'contact' => 'home#contact', as: 'contact_home'
 
   constraints :subdomain => 'store' do
